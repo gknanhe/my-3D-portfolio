771b8c2b73bf066ed2728ce3676c2ce7a8f0bd22
gknanhe <cse18gknanhejd@gmail.com>
2023-09-26 09:16:57 +0530

    added loader when model loads

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
+import { progress } from "framer-motion";
 
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+  const { progress } = useProgress();
 
-export default Loader
+  // Convert progress to a number (if it's not already) and check for NaN
+  const numericProgress = parseFloat(progress);
+  const formattedProgress = isNaN(numericProgress)
+    ? "Loading..."
+    : `${numericProgress.toFixed(2)}%`;
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#f1f1f1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {formattedProgress}
+      </p>
+    </Html>
+  );
+};
+
+export default Loader;
