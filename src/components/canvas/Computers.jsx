8a0d713e7a9961e49d542f39ac8f005d5321604d
gknanhe <cse18gknanhejd@gmail.com>
2023-09-26 07:51:41 +0530

    initialised project

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,13 @@
+import React, { Suspense, useEffect, useState } from "react";
+//alow to somethin place on it
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+// useGLTF let use use and import 3d models
+
+import CanvasLoader from "../Loader";
+
+const ComputersCanvas = () => {
+  return <div></div>;
+};
+
+export default ComputersCanvas;
